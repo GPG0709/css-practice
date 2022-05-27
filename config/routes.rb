de1e10d20d23c54173745af3806e011c6142b219
GPG0709 <guillermo.puga@kellogg.northwestern.edu>
2022-05-27 21:31:08 +0000

    v1

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get "/", :controller => "tacos", :action => "index"
+  get "/", :controller => "dahsboard", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
+  resources "dashboard"
+  resources "projects"
+  resources "sessions"
+  resources "users"
 end
